8856f61cf52a26615f586801729b5bf8bda23cf2
neliodossantos <100626490+neliodossantos@users.noreply.github.com>
2023-05-28 02:02:06 +0100

    conn.js

--- database/conn.js
+++ database/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function main()
 {
-    const url = 'mongodb://localhost:27017/astro';
+    const url = "mongodb+srv://neliodossantos15:neliodossantos15@cluster0.hvhxgye.mongodb.net/?authSource=astro&authMechanism=SCRAM-SHA-1" 
     const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true
