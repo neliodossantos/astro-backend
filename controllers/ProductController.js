b70298b522d6b81161db711af50fe171a251a142
Nelio dos Santos <neliodossantos15@gmail.com>
2023-05-28 16:17:25 +0100

    commit

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,41 +1,31 @@
-const { Category } = require("../models/Category");
+const { Category: CategoryModel} = require("../models/Category");
 const { Product : ProductModel } = require("../models/Product");
-const mongoose = require("mongoose");
 const path = require("path");
 const upload = require("../config/multer");
 const { response } = require("express");
 const productController = {
     index: async(req,res) => {
-        const response = await ProductModel.find().populate('category');
+        const response = await ProductModel.find();
         if (!response) res.status(404).json({response , msg: "Erro ao trazer os dados"});
         res.status(200).json({response,msg:"Sucesso ao trazer o dados"});
     },
     create : async(req,res) => {
-            const {nome , descricao , preco ,categoryId , owner} = req.body;
-            CategoryModel.findOne({ 'categoryId': categoryId }, (err, result) => {
-                if (err) { console.log('Error at finding categoryId ::', err); res.send(err) }
-                if (result) {
-                    const { path } = req.file;
-                     const newProduct = new ProductModel({
-                        nome: nome,
-                        descricao : descricao,
-                        preco: preco,
-                        category : mongoose.Types.ObjectId(result.categoryId),
-                        image : path,
-                        owner : owner,
-                    });
-                    newProduct.save((err,result) => {
-                        if (err) { console.log('Error at saving new blog ::', err); res.send(err) }
-                        else{
-                            console.log('Successfully saved new blog'); res.send(result)
-                        }
-                    })
-                    res.status(201).json({response,msg:"Produto criado com sucesso!"});
-                } else {
-                    console.log('No category found for ::', req.body.category)
-                    res.send('No category found')
-                }
-            })
+        try {
+             const category = await CategoryModel.findById(req.body.category);
+             if(!category) res.status(404).send("Erro nao encontrou a categoria");
+             const { path } = req.file;
+             const response = await ProductModel.create({
+                nome: req.body.nome,
+                descricao : req.body.descricao,
+                preco: req.body.preco,
+                category : req.body.category,
+                image : path,
+                userId : req.body.idUser,
+            });
+            res.status(201).json({response,msg:"Produto criado com sucesso!"});
+        } catch (error){
+            console.log(error);
+        }
     },
     update : async (req,res)=> {
         const getId = await ProductModel.findById(req.params.id)
