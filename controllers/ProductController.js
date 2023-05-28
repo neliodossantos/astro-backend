f54e300c534fbc96ce3b7ca62ddb02faa0b7d565
Nelio dos Santos <neliodossantos15@gmail.com>
2023-05-28 16:14:02 +0100

    commit

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -11,23 +11,31 @@ const productController = {
         res.status(200).json({response,msg:"Sucesso ao trazer o dados"});
     },
     create : async(req,res) => {
-        try {
-            const {nome , descricao , preco ,categoryId ,userId} = req.body;
-             const findCategoryId =await Category.findById(categoryId);
-             if(!findCategoryId) res.status(404).send("Erro nao encontrou a categoria");
-             const { path } = req.file;
-             const response = await ProductModel.create({
-                nome: nome,
-                descricao : descricao,
-                preco: preco,
-                category : mongoose.Types.ObjectId(result.categoryId),
-                image : path,
-                userId : userId,
-            });
-            res.status(201).json({response,msg:"Produto criado com sucesso!"});
-        } catch (error){
-            console.log(error);
-        }
+            const {nome , descricao , preco ,categoryId , owner} = req.body;
+            CategoryModel.findOne({ 'categoryId': categoryId }, (err, result) => {
+                if (err) { console.log('Error at finding categoryId ::', err); res.send(err) }
+                if (result) {
+                    const { path } = req.file;
+                     const newProduct = new ProductModel({
+                        nome: nome,
+                        descricao : descricao,
+                        preco: preco,
+                        category : mongoose.Types.ObjectId(result.categoryId),
+                        image : path,
+                        owner : owner,
+                    });
+                    newProduct.save((err,result) => {
+                        if (err) { console.log('Error at saving new blog ::', err); res.send(err) }
+                        else{
+                            console.log('Successfully saved new blog'); res.send(result)
+                        }
+                    })
+                    res.status(201).json({response,msg:"Produto criado com sucesso!"});
+                } else {
+                    console.log('No category found for ::', req.body.category)
+                    res.send('No category found')
+                }
+            })
     },
     update : async (req,res)=> {
         const getId = await ProductModel.findById(req.params.id)
