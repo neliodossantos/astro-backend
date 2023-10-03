525c5767cb7e3b783ab9ecd09022e1820f437676
nelio <neliodossantos15@gmail>
2023-10-03 04:43:40 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/router');
+const cors = require('cors');
 // forma de ler json
 app.use(express.urlencoded({extended:true,}),)
 app.use(express.json());
